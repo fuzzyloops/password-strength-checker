4e4b5550b704f395d0f3969308f4b2415da0dad5
Hunter Spradlyn <Dropsuitcaptain@gmail.com>
2025-04-01 20:18:18 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    //range of special characters 33-63, 91-96, 123-126
+    //range of uppercase characters 65-90
+    //range of lowercase characters 97-122
+
+    std::string password;
+
+    std::cout << "What password would you like to check the stength of?" << std::endl;
+    std::cin >> password;
+}
